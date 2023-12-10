6855768ee1b403d6c1aab4d99675b593e78192d2
MohamedKamel-Mk <mohamed.k0608@gmail.com>
2023-12-10 19:07:55 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var arrayOfQuotes = [{
+    'author': 'Jim Rohn',
+    'quote': 'Beware of what you become in pursuit of what you want.'
+},{
+    'author': 'Epictetus', 
+    'quote': 'It\'s not what happens to you, but how you react to it that matters.'
+},{
+    'author': 'Frank Sinatra', 
+    'quote': 'The best revenge is massive success.'
+}, {
+    'author': 'Wayne Gretzy', 
+    'quote': 'You miss 100% of the shots you don\'t take.'
+},{
+    'author': 'Nelson Mandela', 
+    'quote': 'Resentment is like drinking poison and waiting for your enemies to die.'
+},{
+    'author': 'Elbert Hubbard', 
+    'quote': 'Do not take life too seriously. You will not get out alive.'
+},]
+
+function generateQuote (){
+    var floor = (Math.floor(Math.random()*arrayOfQuotes.length));
+    document.querySelector(`#quoteOutPut`).textContent = `\"${arrayOfQuotes[floor].quote}\"`;
+    document.querySelector('#authorOutPut').textContent = `--${arrayOfQuotes[floor].author}`;
+}
